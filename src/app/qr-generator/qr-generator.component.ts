f8d14fdd33b45c8aee76705bb834edbb506570bf
talxdo <mccn.tala@gmail.com>
2024-10-24 12:55:46 -0400

    animacion ok

--- src/app/qr-generator/qr-generator.component.ts
+++ src/app/qr-generator/qr-generator.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { QRCodeModule } from 'angularx-qrcode';
 @Component({
   selector: 'app-qr-generator',
   standalone: true,
   imports: [QRCodeModule, 
             ReactiveFormsModule,
-            CommonModule
+            CommonModule,
             ],
   templateUrl: './qr-generator.component.html',
   styleUrl: './qr-generator.component.css'
@@ -18,8 +19,18 @@ export class QrGeneratorComponent {
   })
 
   public myAngularxQrCode: string = "";
+  showQr : boolean = false;
 
   generateQr(){
     this.myAngularxQrCode = this.myForm.get('url')?.value || "";
+    this.showQr = false;
+    setTimeout(() => {
+      this.showQr = true;
+    }, 0.4);
+
+  }
+  resetQr(){
+    this.myForm.reset();
+    this.showQr = false;
   }
 }
