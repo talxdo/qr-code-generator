d556a0ddffcef8009d01eb152aac7b7343cf8629
talxdo <mccn.tala@gmail.com>
2024-10-24 12:00:44 -0400

    reactivo ok

--- src/app/qr-generator/qr-generator.component.ts
+++ src/app/qr-generator/qr-generator.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { QRCodeModule } from 'angularx-qrcode';
-
 @Component({
   selector: 'app-qr-generator',
   standalone: true,
-  imports: [QRCodeModule, ReactiveFormsModule, CommonModule],
+  imports: [QRCodeModule, 
+            ReactiveFormsModule,
+            CommonModule
+            ],
   templateUrl: './qr-generator.component.html',
   styleUrl: './qr-generator.component.css'
 })
@@ -14,6 +16,7 @@ export class QrGeneratorComponent {
   public myForm : FormGroup = new FormGroup({
     url : new FormControl('', Validators.required)
   })
+
   public myAngularxQrCode: string = "";
 
   generateQr(){
