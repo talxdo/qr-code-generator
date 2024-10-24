5ea5ba3d297eaaa6b5bd21be967fe063a1a4c0fb
talxdo <mccn.tala@gmail.com>
2024-10-24 11:24:34 -0400

    qr generator ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QrGeneratorComponent } from './qr-generator/qr-generator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QrGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/qr-generator/qr-generator.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { QRCodeModule } from 'angularx-qrcode';
+
+@Component({
+  selector: 'app-qr-generator',
+  standalone: true,
+  imports: [QRCodeModule, ReactiveFormsModule, CommonModule],
+  templateUrl: './qr-generator.component.html',
+  styleUrl: './qr-generator.component.css'
+})
+export class QrGeneratorComponent {
+  public myForm : FormGroup = new FormGroup({
+    url : new FormControl('', Validators.required)
+  })
+  public myAngularxQrCode: string = "";
+
+  generateQr(){
+    this.myAngularxQrCode = this.myForm.get('url')?.value || "";
+  }
+}
